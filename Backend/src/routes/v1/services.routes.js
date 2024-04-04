c4e3e5e2936101a3d77bffb78c6b9748eb77dc8d
Angel Christian <an321060@dal.ca>
2024-04-03 22:27:17 -0300

    fix: modified the service creation api to restrict the usage

--- Backend/src/routes/v1/services.routes.js
+++ Backend/src/routes/v1/services.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createService, getAllServices, getServiceById, updateService, deleteService, getServicesByPartialHint } from '../../controllers/services.controller.js';
+import { authenticate, isSeller } from '../../middleware/auth.js';
 
 const router = express.Router();
 
 
 router.get('/search', getServicesByPartialHint);
-router.post('/', createService);
+router.post('/',authenticate,isSeller,createService);
 router.get('/', getAllServices);
 router.get('/:id', getServiceById);
 router.put('/:id', updateService);
